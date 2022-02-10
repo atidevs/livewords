3dec7ad5e55f0b4cb5fb5e5da9ba37ab91a5ab6e
Atmane TAOUSSI <atmane.taoussi1@ibm.com>
2022-02-10 12:41:19 +0100

    [FEAT] Implement live text recognition using ml kit and camerax

--- app/src/main/java/com/atidevs/livewords/livebasedtranslation/LiveTranslateFragment.kt
+++ app/src/main/java/com/atidevs/livewords/livebasedtranslation/LiveTranslateFragment.kt
@@ -1,10 +1,13 @@
 package com.atidevs.livewords.livebasedtranslation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -12,7 +15,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.atidevs.livewords.databinding.FragmentLiveTranslateBinding
+import com.google.android.gms.tasks.Task
 import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class LiveTranslateFragment() : Fragment() {
 
@@ -22,6 +31,10 @@ class LiveTranslateFragment() : Fragment() {
     lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
     private var liveTranslateViewModel: LiveTranslateViewModel? = null
+
+    private lateinit var imageAnalysis: ImageAnalysis
+
+    private lateinit var detector: TextRecognizer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +48,7 @@ class LiveTranslateFragment() : Fragment() {
     ): View? {
         _binding = FragmentLiveTranslateBinding.inflate(inflater, container, false)
         liveTranslateViewModel = ViewModelProvider(this)[LiveTranslateViewModel::class.java]
+        initTextRecognition()
         initCameraPreview()
         return binding.root
     }
@@ -46,6 +60,7 @@ class LiveTranslateFragment() : Fragment() {
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
+    @SuppressLint("UnsafeOptInUsageError")
     private fun bindPreview(cameraProvider: ProcessCameraProvider) {
         val preview: Preview = Preview.Builder().build()
 
@@ -56,11 +71,51 @@ class LiveTranslateFragment() : Fragment() {
 
         preview.setSurfaceProvider(binding.previewView.surfaceProvider)
 
-        val camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        imageAnalysis =
+            ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(requireContext())) { imageProxy ->
+            val image = imageProxy.image
+            if (image != null) {
+                val inputImage =
+                    InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
+                recognizeText(inputImage)
+                    .addOnCompleteListener {
+                        imageProxy.close()
+                    }
+                    .addOnFailureListener {
+                        imageProxy.close()
+                    }
+            }
+        }
+
+        val camera = cameraProvider.bindToLifecycle(
+            this as LifecycleOwner,
+            cameraSelector,
+            imageAnalysis,
+            preview
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initTextRecognition() {
+        detector = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        lifecycle.addObserver(detector)
+    }
+
+    private fun recognizeText(image: InputImage): Task<Text> {
+        return detector.process(image)
+            .addOnSuccessListener {
+                binding.sourceText.text = it.text
+            }
+            .addOnFailureListener { exception ->
+                Log.e("LiveTranslationFragment", "Text recognition error", exception)
+                // Handle error message!
+            }
     }
 }
