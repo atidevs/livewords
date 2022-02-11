2f51f3a0eebd526b2f4c5e12af7c80cafa6a3a6b
Atmane TAOUSSI <atmane.taoussi1@ibm.com>
2022-02-11 16:18:38 +0100

    [FEAT][UI] Implement camera preview cropping by drawing on top of the surface's canvas

--- app/src/main/java/com/atidevs/livewords/livebasedtranslation/LiveTranslateFragment.kt
+++ app/src/main/java/com/atidevs/livewords/livebasedtranslation/LiveTranslateFragment.kt
@@ -1,24 +1,29 @@
 package com.atidevs.livewords.livebasedtranslation
 
-import android.annotation.SuppressLint
+import android.graphics.*
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.*
 import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
+import com.atidevs.livewords.R
 import com.atidevs.livewords.common.ScopedExecutor
 import com.atidevs.livewords.databinding.FragmentLiveTranslateBinding
 import com.google.common.util.concurrent.ListenableFuture
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.ln
+import kotlin.math.max
+import kotlin.math.min
 
 class LiveTranslateFragment : Fragment() {
 
@@ -35,6 +40,13 @@ class LiveTranslateFragment : Fragment() {
     private lateinit var cameraExecutor: Executor
     private lateinit var scopedExecutor: ScopedExecutor
 
+    companion object {
+        const val RATIO_4_BY_3 = 4.0 / 3.0
+        const val RATIO_16_BY_9 = 16.0 / 9.0
+        const val WIDTH_CROP_PERCENT = 8
+        const val HEIGHT_CROP_PERCENT = 74
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
@@ -46,9 +58,32 @@ class LiveTranslateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLiveTranslateBinding.inflate(inflater, container, false)
+
         initCameraPreview()
-        init()
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.surfaceView.apply {
+            setZOrderOnTop(true)
+            holder.setFormat(PixelFormat.TRANSPARENT)
+            holder.addCallback(object : SurfaceHolder.Callback {
+                override fun surfaceCreated(holder: SurfaceHolder) {
+                    liveTranslateViewModel.imageCropPercentages.observe(viewLifecycleOwner) {
+                        drawOverlay(holder, it.first, it.second)
+                    }
+                }
+
+                override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {}
+
+                override fun surfaceDestroyed(p0: SurfaceHolder) {}
+            })
+        }
+
+        init()
     }
 
     private fun init() {
@@ -61,43 +96,121 @@ class LiveTranslateFragment : Fragment() {
     private fun initCameraPreview() {
         cameraExecutor = Executors.newCachedThreadPool()
         scopedExecutor = ScopedExecutor(cameraExecutor)
+
+        liveTranslateViewModel.executor = cameraExecutor
+
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
-            bindPreview(cameraProvider)
+            bindCameraPreview(cameraProvider)
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
-    @SuppressLint("UnsafeOptInUsageError")
-    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
-        val preview: Preview = Preview.Builder().build()
+    private fun bindCameraPreview(cameraProvider: ProcessCameraProvider) {
+        val metrics = DisplayMetrics().also { binding.previewView.display.getRealMetrics(it) }
+
+        val screenAspectRatio = getAspectRatio(metrics.widthPixels, metrics.heightPixels)
+
+        val rotation = binding.previewView.display.rotation
+
+        val preview: Preview = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+
+        imageAnalysis = ImageAnalysis.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build().apply {
+                setAnalyzer(
+                    scopedExecutor,
+                    TextAnalyzer(
+                        requireContext(),
+                        lifecycle,
+                        cameraExecutor,
+                        liveTranslateViewModel.sourceText
+                    )
+                )
+            }
 
         val cameraSelector: CameraSelector = CameraSelector.Builder()
             .requireLensFacing(
                 CameraSelector.LENS_FACING_BACK
             ).build()
 
-        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        try {
+            cameraProvider.unbindAll()
 
-        imageAnalysis =
-            ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-
-        imageAnalysis.setAnalyzer(
-            scopedExecutor,
-            TextAnalyzer(
-                requireContext(),
-                lifecycle,
-                cameraExecutor,
-                liveTranslateViewModel.sourceText
+            camera = cameraProvider.bindToLifecycle(
+                this as LifecycleOwner,
+                cameraSelector,
+                imageAnalysis,
+                preview
             )
-        )
 
-        camera = cameraProvider.bindToLifecycle(
-            this as LifecycleOwner,
-            cameraSelector,
-            imageAnalysis,
-            preview
-        )
+            preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        } catch (e: IllegalStateException) {
+        }
+    }
+
+    private fun getAspectRatio(width: Int, height: Int): Int {
+        val previewRatio = ln(max(width, height).toDouble() / min(width, height))
+        if (
+            abs(previewRatio - ln(RATIO_4_BY_3)) <= abs(previewRatio - ln(RATIO_16_BY_9))
+        ) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+    private fun drawOverlay(
+        holder: SurfaceHolder,
+        heightCropPercent: Int,
+        widthCropPercent: Int
+    ) {
+        val canvas = holder.lockCanvas()
+        val backgroundPaint = Paint().apply {
+            alpha = 120
+        }
+
+        canvas.drawPaint(backgroundPaint)
+
+        val rectPaint = Paint()
+        rectPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        rectPaint.style = Paint.Style.FILL
+        rectPaint.color = Color.WHITE
+
+        val outlinePaint = Paint()
+        outlinePaint.style = Paint.Style.STROKE
+        outlinePaint.color = Color.WHITE
+        outlinePaint.strokeWidth = 5f
+
+        val surfaceWidth = holder.surfaceFrame.width()
+        val surfaceHeight = holder.surfaceFrame.height()
+
+        val cornerRadius = 25f
+        val recTop = surfaceHeight * heightCropPercent / 2 / 100f
+        val rectLeft = surfaceWidth * widthCropPercent / 2 / 100f
+        val rectRight = surfaceWidth * (1 - widthCropPercent / 2 / 100f)
+        val rectBottom = surfaceHeight * (1 - heightCropPercent / 2 / 100f)
+        val rect = RectF(rectLeft, recTop, rectRight, rectBottom)
+
+        canvas.drawRoundRect(rect, cornerRadius, cornerRadius, rectPaint)
+
+        canvas.drawRoundRect(rect, cornerRadius, cornerRadius, outlinePaint)
+
+        val textPaint = Paint()
+        textPaint.color = Color.WHITE
+        textPaint.textSize = 50F
+
+        val overlayText = getString(R.string.center_text_label)
+        val textBounds = Rect()
+        textPaint.getTextBounds(overlayText, 0, overlayText.length, textBounds)
+        val textX = (surfaceWidth - textBounds.width()) / 2f
+        val textY = rectBottom + textBounds.height() + 15f
+
+        canvas.drawText(overlayText, textX, textY, textPaint)
+        holder.unlockCanvasAndPost(canvas)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/atidevs/livewords/livebasedtranslation/LiveTranslateViewModel.kt
+++ app/src/main/java/com/atidevs/livewords/livebasedtranslation/LiveTranslateViewModel.kt
@@ -3,10 +3,18 @@ package com.atidevs.livewords.livebasedtranslation
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.atidevs.livewords.livebasedtranslation.LiveTranslateFragment.Companion.HEIGHT_CROP_PERCENT
+import com.atidevs.livewords.livebasedtranslation.LiveTranslateFragment.Companion.WIDTH_CROP_PERCENT
 import com.google.mlkit.nl.languageid.LanguageIdentification
 import com.google.mlkit.nl.languageid.LanguageIdentificationOptions
+import java.util.concurrent.Executor
 
 class LiveTranslateViewModel : ViewModel() {
+
+    lateinit var executor: Executor
+
+    val imageCropPercentages = MutableLiveData<Pair<Int, Int>>()
+        .apply { value = Pair(HEIGHT_CROP_PERCENT, WIDTH_CROP_PERCENT) }
 
     private var languageIdentifier =
         LanguageIdentification.getClient(LanguageIdentificationOptions.Builder().build())
