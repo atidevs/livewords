2144db3d33b09e9b5bf40423bdb5849bfa5a08a4
Atmane TAOUSSI <atmane.taoussi1@ibm.com>
2022-02-11 12:42:58 +0100

    [FEAT] Prepare text based experience by implementing its fragment and viewmodel

--- app/src/main/java/com/atidevs/livewords/textbasedtranslation/TextTranslateFragment.kt
+++ app/src/main/java/com/atidevs/livewords/textbasedtranslation/TextTranslateFragment.kt
@@ -1,6 +1,31 @@
 package com.atidevs.livewords.textbasedtranslation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.atidevs.livewords.databinding.FragmentTextTranslateBinding
 
-class TextTranslateFragment: Fragment() {
+class TextTranslateFragment : Fragment() {
+
+    private var _binding: FragmentTextTranslateBinding? = null
+    private val binding get() = _binding!!
+
+    private val textTranslateViewModel: TextTranslateViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTextTranslateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
---
+++ app/src/main/java/com/atidevs/livewords/textbasedtranslation/TextTranslateViewModel.kt
@@ -0,0 +1,5 @@
+package com.atidevs.livewords.textbasedtranslation
+
+import androidx.lifecycle.ViewModel
+
+class TextTranslateViewModel: ViewModel()
