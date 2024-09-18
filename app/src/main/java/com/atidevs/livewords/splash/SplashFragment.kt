836052e5f395142af9d0a24d4ad0df3966391e24
Atmane TA <42352387+atidevs@users.noreply.github.com>
2024-09-18 14:41:41 +0200

    [TECH] Bump target SDK to 33 and update app icons

--- app/src/main/java/com/atidevs/livewords/splash/SplashFragment.kt
+++ app/src/main/java/com/atidevs/livewords/splash/SplashFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.atidevs.livewords.MainActivity
 import com.atidevs.livewords.common.Destination
